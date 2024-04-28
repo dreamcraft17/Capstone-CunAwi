4de0116b38f5ebbd1a4932a1e605c81aa8cd8436
Dozer Napitupulu <dozerfernando.napitupulu@mattel.com>
2024-04-28 19:34:10 +0700

    baru lagi

--- app/Http/Controllers/ProjectListController.php
+++ app/Http/Controllers/ProjectListController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\DashboardController;
+use App\Models\Data;
 // use App\Http\Controllers\ProjectListController;
 
 class ProjectListController extends Controller
 {
     public function projectlist(){
-        return view("pages.projectlist");
+        $projects = Data::all(); 
+    
+        return view("pages.projectlist", compact('projects')); 
     }
+    
 
      public function newproject(){
         return view("pages.newproject");
@@ -22,4 +26,15 @@ class ProjectListController extends Controller
     public function projectdetail(){
         return view("pages.projectdetail");
     }
+
+    public function delete($project)
+    {
+        $project = Data::findOrFail($project);
+        $project->delete();
+    
+        return redirect()->route('projectlist')->with('success', 'Project deleted successfully');
+    }
+    
+    
+
 }
---
+++ database/migrations/2024_04_28_115921_create_data_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data', function (Blueprint $table) {
+            $table->id();
+            $table->string('projectID');
+            $table->string('assortment');
+            $table->string('productID');
+            $table->string('toyName');
+            $table->string('category');
+            $table->string('designer');
+            $table->string('pe');
+            $table->string('meeting');
+            $table->string('start_date');
+            $table->integer('month');
+            $table->string('finish_cmt');
+            $table->string('finish_act');
+            $table->string('adherence');
+            $table->string('status');
+            $table->double('lead_time');
+            $table->string('remarks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data');
+    }
+};
